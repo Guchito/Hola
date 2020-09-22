fe04a1521b75bb4ac54bb82a242612adf94081e1
vaguirre <aguirregonzalezvioleta@gmail.com>
2020-09-22 00:25:01 -0300

    comentario nuevo

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 console.log ('soy una mimosa')
 console.log ('alo alo')
 console.log ('soy una burra, gracias Mati')
+
+
+/* 
+
+git pull 
+git checkout -b violeMimosa 
+git add app.js 
+git commit -m "nueva branch creada por viole"
+git push --set-upstream origin violeMimosa
+
+*/ 
+
